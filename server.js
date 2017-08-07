04ad33995db8a1b83f488fb229860aabb514b3ef
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-07 13:56:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get("/article-one",function(req,res)
 {
-   res.sendFile("Article one","ui","article-one.html"); 
+   res.sendFile(__dirname,"ui","article-one.html"); 
 });
 app.get("/article-two",function(req,res)
 {
