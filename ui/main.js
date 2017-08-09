b7f3a9082af3367247bd9f18bf0bde3aaec07122
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-09 20:59:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element =document.getElementById("main_text");
 
 element.innerHTML="<h2>SAMPLE VALUE</h2>";
+
+
+var img=document.getElementById("im");
+img.onclick=function()
+{
+  img.style.marginLeft='100px';  
+};
