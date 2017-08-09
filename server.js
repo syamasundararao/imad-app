b577e769b716e310460e470bb5e3c1d5f914bf55
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-09 20:18:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ function createTemplate(data)
     var title=data.title;
 var heading=data.heading;
 var date=data.date;
-var content=data.contentt;
+var content=data.content;
 
 var htmlTemplate=`
 <html>
