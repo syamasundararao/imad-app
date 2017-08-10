5d2dc24b1d000ac6d1dcb3d6e79fb8dd0ec6cdb1
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-10 14:42:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+
 var names=[];
 app.get('/submit-name',function(req,res)
 {
@@ -117,6 +118,9 @@ app.get('/submit-name',function(req,res)
    
    res.send(JSON.stringify(names));
 });
+
+
+
 app.get("/:articleName",function(req,res)
 {
     var articleName=req.params.articleName;
