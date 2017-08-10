ed06de81c125190fe3f489fe2a3698f243b72b85
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-10 14:51:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -53,8 +53,7 @@ button.onclick=function()
 };
 
 //Submit Name
-var nameInput=document.getElementById("name");
-var name=nameInput.value;
+
 var submit=document.getElementById("submit_btn");
 submit.onclick=function()
 {
@@ -81,11 +80,13 @@ submit.onclick=function()
     }
    
     
-   //ar names=["name1","name2","name3","name4"];
-  }
+var nameInput=document.getElementById("name");
+var name=nameInput.value;
+  
    
     request.open("GET","http://syam5492009.imad.hasura-app.io/submit-name?name"+name,true);
   request.send(null);
+  }
     
 };
 
