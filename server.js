41b0a257a8c9e4d052c976cd3c2f75128ee5e892
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-10 14:24:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -137,7 +137,7 @@ app.get('/ui/madi.png', function (req, res) {
 var names=[];
 app.get('/submit-name/:name',function(req,res)
 {
-    var name=req.param.name;
+    var name=req.params.name;
    names.push(name);
    //json
    
