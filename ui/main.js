67aa38ec02fd90f82d54851941ab20490bd7211b
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-10 14:08:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,16 +24,31 @@ function fun()
 }*/
 
 
-counter=0;
+//counter=0;
 //Counter Code
 var button=document.getElementById("counter");
 alert(button);
 button.onclick=function()
 {
+  var request=new XMLHttpRequest();
   
-  counter=counter+1;
-  var span=document.getElementById("count");
-  span.innerHTML=counter.toString();
+  request.onreadystatechange=function()
+  {
+    if(request.readyState==XMLHttpRequest.DONE)
+    {
+        if(request.status==200)
+        {
+            var counter=request.responseText;
+            var span=document.getElementById("count");
+            span.innerHTML=counter.toString();
+        }
+    }
+  };
+  
+  
+  //counter=counter+1;
+  request.open("GET","http://syam5492009.imad.hasura-app.io/counter")
+  request.send(null);
  
 };
 
