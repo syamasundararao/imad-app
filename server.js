67506267bfa248aa1530e213a0fdcb03c653b287
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-11 20:49:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var morgan = require('morgan'); //request and response
 var path = require('path');
 //var http=require('http');
 
-//var pool=require('pg').Pool;
+var pool=require('pg').Pool;
 
 /*
 var config={
