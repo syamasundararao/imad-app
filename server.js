38672c128292a8b77e36ac80d0c4ab0592261cc4
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-16 11:27:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -163,7 +163,7 @@ app.post('/create-user',function(req,res)
         res.status(500).send(err.toString());}
         else{
             
-            res.send('User Successfully registered"+username);}
+            res.send("User Successfully registered"+username);}
    });
 });
 
