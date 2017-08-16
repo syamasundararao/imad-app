2ac691d62d313fb949677c3b7f731534dd298dfd
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-16 11:13:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -152,7 +152,7 @@ app.post('/create-user',function(req,res)
 {
     var username=req.body.username;
     var password=req.body.password;
-   var salt=crypto.getRandomBytes(128).toString('hex');
+   var salt=crypto.randomBytes(128).toString('hex');
    var dbString=hash(password,salt);
    pool.query("INSERT INTO test_user(username,password) values($1,$2)",{username,dbString},function(req,res)
    {
