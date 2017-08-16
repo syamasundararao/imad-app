4eb09322615ac5cb035dc1d3a1ed488e8e627c35
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-16 20:42:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -141,7 +141,8 @@ var pwdInput=document.getElementById("pwd").value;
   alert(nameInput);
    
   request.open("POST","http://syam5492009.imad.hasura-app.io/login",true);
-  request.send(JSON.stringfy(username:username,password:password));
+  request.setRequestHeader("Content-Type:application/json");
+  request.send(JSON.stringify({username:username,password:password}));
     
 };
 
