cc8ea5650aec97d847837c115125d584c077a6fc
syamasundararao <syamasundararao@pvpsiddhartha.ac.in>
2017-08-16 11:23:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -228,13 +228,13 @@ app.get('/submit-name',function(req,res)
 });
 
 
-
+/*
 app.get("/:articleName",function(req,res)
 {
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
    
-});
+});*/
 
 
 /*app.get("/article-two",function(req,res)
